8ee8a19033d136b51a432356e57a8cc8e556b8dc
haha479 <self479@163.com>
2020-12-27 16:10:26 +0800

    done

--- 10-杨辉三角形.c
+++ 10-杨辉三角形.c
@@ -1,18 +1,35 @@
 #include<stdio.h>
-
+/*
+打印杨辉三角形前10行 
+如:
+1
+1 1
+1 2 1 
+1 3 3 1
+1 4 6 4 1
+*/
 
 int main(){
 	
-	
+	int arr[10][10]; 
 	int i,j;
-	for(i=1;i<=6;i++){
-		for(j=1;j<=i;j++){
-			
-			
+	for(i=0;i<10;i++){
+		for(j=0;j<=i;j++){
+			if(j==0 || i==j){
+				arr[i][j]=1;
+			}
+			else{
+				arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
+			}
 		}
-		printf("\n");
 	}
 	
+	for(i=0;i<10;i++){
+		for(j=0;j<=i;j++){
+			printf("%d\t",arr[i][j]);
+		}
+		printf("\n");
+	}	
 	
 	
 	
